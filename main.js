f3970e36d6e7a0fe2603441976e2614152746fa7
eduardocardozoscheles <148091858+eduardocardozoscheles@users.noreply.github.com>
2024-05-07 08:54:04 -0300

    69

--- main.js
+++ main.js
@@ -9,15 +9,6 @@ function verificarTime() {
     
     if (respostaTime.toLowerCase() === "real") {
       alert("Isso mesmo! O REAL MEDRID é o MAIOR time!");
-      continuarPerguntando = true; // Sai do loop após a resposta correta
-    } else {
-      alert("Não é isso! Tente novamente.");
-    }
-    var respostaTime = prompt("no ceu tem pão");
-
-    
-    if (respostaTime.toLowerCase() === "real") {
-      alert("Isso mesmo! O REAL MEDRID é o MAIOR time!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
